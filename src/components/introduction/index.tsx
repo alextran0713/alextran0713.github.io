d9c253bc6ebf644558cce0e2774312740769a274
Alex Tran <quyen137@csu.fullerton.edu>
2023-07-09 16:39:10 -0700

    feat(Intro): Setup intro with about me

---
+++ src/components/introduction/index.tsx
@@ -0,0 +1,15 @@
+import { Container } from "react-bootstrap";
+import styles from "./index.module.scss";
+import Profile from "../profile";
+import AboutMe from "../about-me";
+
+const Intro = () => {
+  return (
+    <div className={styles?.["container"]}>
+      <Profile />
+      <AboutMe />
+    </div>
+  );
+};
+
+export default Intro;
--- src/pages/main.page.tsx
+++ src/pages/main.page.tsx
@@ -1,3 +1,4 @@
+import BodyContent from "../components/body";
 import NavigationBar from "../components/navbar";
 import styles from "./main.module.scss";
 
@@ -5,6 +6,7 @@ const MainPage = () => {
   return (
     <div className={styles?.["container"]}>
       <NavigationBar />
+      <BodyContent />
     </div>
   );
 };
