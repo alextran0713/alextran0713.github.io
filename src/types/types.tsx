3529d79d2fd94517eac0b4371127d0038b5bd7bb
Alex Tran <quyen137@csu.fullerton.edu>
2023-07-13 22:52:33 -0700

    feat(work experience): Complete work experiences section with data file

--- src/types/types.tsx
+++ src/types/types.tsx
@@ -1,6 +1,9 @@
+import { JobDate } from "../components/job-item";
+
 export interface JobInfo {
   jobTitle: string;
   jobDescriptions: string[];
   companyName: string;
   companyLogo?: string;
+  duration: JobDate;
 }
